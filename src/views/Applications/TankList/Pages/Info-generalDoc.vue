8bcef123dc44c226d052f63e285f865fa5724d76
Peerapong <peerapong.thammakaew@dacon-inspection.com>
2022-10-28 16:21:10 +0700

    tank info (no API)

--- src/views/Applications/TankList/Pages/Info-generalDoc.vue
+++ src/views/Applications/TankList/Pages/Info-generalDoc.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="page-section">
-    <div class="page-section-label">General Document</div>
     <DxDataGrid
       id="data-grid-style"
-      :data-source="courseList"
+      :data-source="pidList"
       :selection="{ mode: 'single' }"
       :hover-state-enabled="true"
       :allow-column-reordering="true"
@@ -11,34 +10,36 @@
       :show-row-lines="true"
       :row-alternation-enabled="false"
       @exporting="EXPORT_DATA"
-      :word-wrap-enabled="true"
     >
+      <DxToolbar>
+        <DxItem location="before" template="table-header" />
+      </DxToolbar>
       <DxColumn
         data-field="created_time"
         :width="0"
         caption=""
         sort-order="asc"
       />
-      <DxColumn data-field="course_no" caption="Course No" />
-      <DxColumn
-        data-field="nominal_shell_thk"
-        caption="Nominal Shell Thk (mm)"
-      />
-      <DxColumn data-field="accu_height" caption="Accumulate Height" />
-      <DxColumn data-field="tank_material" caption="Tank Material" />
-      <DxColumn data-field="material_type" caption="Material Type" />
-      <DxColumn data-field="y" caption="y" />
-      <DxColumn data-field="t" caption="t" />
-      <DxColumn data-field="height_hydro" caption="Height Hydro" />
-      <DxColumn data-field="height_prod" caption="Height Prod" />
-      <DxColumn data-field="tretire_hydro" caption="tretire Prod" />
-      <DxColumn data-field="height_prod" caption="tretire Prod" />
-      <DxColumn :width="50" caption="" cell-template="cell-button-set" />
+      <DxColumn data-field="file_name" caption="File Name" />
+      <DxColumn data-field="file_type" caption="File Type" :width="120" />
+      <DxColumn data-field="created_time" caption="Uploaded On" :width="200" />
+      <DxColumn :width="110" caption="" cell-template="cell-button-set" />
       <template #cell-button-set="{ data }">
         <div class="table-btn-group">
           <div class="table-btn" v-on:click="VIEW_INFO(data)">
-            <i class="las la-edit blue"></i>
+            <i class="las la-search blue"></i>
           </div>
+          <div class="table-btn" v-on:click="VIEW_INFO(data)">
+            <i class="las la-download green"></i>
+          </div>
+          <div class="table-btn" v-on:click="VIEW_INFO(data)">
+            <i class="las la-trash red"></i>
+          </div>
+        </div>
+      </template>
+      <template #table-header>
+        <div>
+          <div class="page-section-label">General Document</div>
         </div>
       </template>
       <!-- Configuration goes here -->
@@ -78,6 +79,8 @@ import {
   DxScrolling,
   DxColumn,
   DxExport,
+  DxToolbar,
+  DxItem,
 } from "devextreme-vue/data-grid";
 
 export default {
@@ -90,40 +93,28 @@ export default {
     DxScrolling,
     DxColumn,
     DxExport,
+    DxToolbar,
+    DxItem,
   },
   created() {},
   data() {
     return {
-      courseList: [
+      pidList: [
         {
           id: 1,
-          course_no: 1,
-          nominal_shell_thk: 6.35,
-          height: 1.828,
-          accu_height: 1.83,
-          tank_material: null,
-          material_type: "CS",
-          y: 30000,
-          t: 55000,
-          height_hydro: 10.97,
-          height_prod: 10.97,
-          tretire_hydro: 0.17,
-          tretire_prod: 0.19,
+          file_name: "other",
+          file_type: "pdf",
+          file_url: "/wwwroot/file/drawing/tank/file.pdf",
+          created_time: "2022-08-15 08:35:00",
+          created_by: 2,
         },
         {
           id: 2,
-          course_no: 2,
-          nominal_shell_thk: 6.35,
-          height: 1.828,
-          accu_height: 3.66,
-          tank_material: null,
-          material_type: "CS",
-          y: 30000,
-          t: 55000,
-          height_hydro: 10.97,
-          height_prod: 10.97,
-          tretire_hydro: 0.17,
-          tretire_prod: 0.19,
+          file_name: "summary",
+          file_type: "docx",
+          file_url: "/wwwroot/file/drawing/tank/file.pdf",
+          created_time: "2022-10-28 15:35:00",
+          created_by: 3,
         },
       ],
     };
