b661d015918b0b52e95b5f0a522a190a421fd23f
Weerawat S <weerawat.suwattanapiset@dacon-inspection.com>
2022-11-11 17:02:59 +0700

    Update Page.vue

--- src/views/Applications/TankList/Pages/Thickness/Roof/Page.vue
+++ src/views/Applications/TankList/Pages/Thickness/Roof/Page.vue
@@ -2,9 +2,10 @@
   <div class="page-container">
     <div class="page-section">
       <div class="table-wrapper">
+        <button v-on:click="VIEW_TP()">ffffffS</button>
         <DxDataGrid
-          id="data-grid-style"
-          key-expr="id_inspection_record"
+          id="cml-grid"
+          key-expr="id_cml"
           :data-source="dataList.cml"
           :selection="{ mode: 'single' }"
           :hover-state-enabled="true"
@@ -12,18 +13,102 @@
           :show-borders="true"
           :show-row-lines="true"
           :row-alternation-enabled="false"
-          @exporting="EXPORT_DATA"
           :word-wrap-enabled="true"
-          @row-inserted="CREATE_RECORD"
-          @row-updated="UPDATE_RECORD"
-          @row-removed="DELETE_RECORD"
+          @row-inserted="CREATE_CML"
+          @row-updated="UPDATE_CML"
+          @row-removed="DELETE_CML"
         >
           <DxEditing
             :allow-updating="true"
             :allow-deleting="true"
             :allow-adding="true"
+            :use-icons="true"
             mode="row"
           />
+
+          <DxColumn 
+            data-field="roof_row" 
+            caption="Roof row"
+          />
+
+          <DxColumn 
+            data-field="roof_column" 
+            caption="Roof column" 
+          />
+
+          <DxColumn 
+            data-field="t_nom" 
+            caption="tnom (mm)" 
+            format="#,##0.00"
+          />
+
+          <DxColumn 
+            data-field="t_req" 
+            caption="treq (mm)" 
+            format="#,##0.00"
+            :allow-editing="false"
+          />
+
+          <DxColumn
+            data-field="inservice_date"
+            caption="In-service date"
+            data-type="date"
+            format="dd MMM yyyy"
+            sort-order="desc"
+            :width="120"
+          />
+
+          <DxColumn 
+            type="buttons"
+          >
+            <DxButton
+              hint="View TP"
+              icon="search"
+              @click="VIEW_TP"
+            />
+
+            <DxButton
+              name="edit"
+              hint="Edit"
+              icon="edit"
+            />
+
+            <DxButton
+              name="delete"
+              hint="Delete"
+              icon="trash"
+            />
+          </DxColumn>
+
+
+          <!-- Configuration goes here -->
+          <!-- <DxFilterRow :visible="true" /> -->
+          <DxScrolling mode="standard" />
+          <DxSearchPanel :visible="true" />
+          <DxPaging :page-size="10" :page-index="0" />
+          <DxPager
+            :show-page-size-selector="true"
+            :allowed-page-sizes="[5, 10, 20]"
+            :show-navigation-buttons="true"
+            :show-info="true"
+            info-text="Page {0} of {1} ({2} items)"
+          />
+          <DxExport :enabled="true" />
+        </DxDataGrid>
+      </div>
+      <div class="table-wrapper">
+        <DxDataGrid
+          id="data-grid-style"
+          key-expr="id_inspection_record"
+          :data-source="dataList.tp"
+          :selection="{ mode: 'single' }"
+          :hover-state-enabled="true"
+          :allow-column-reordering="true"
+          :show-borders="true"
+          :show-row-lines="true"
+          :row-alternation-enabled="false"
+          :word-wrap-enabled="true"
+        >
           <!-- <DxColumn
         data-field="created_time"
         :width="0"
@@ -40,13 +125,6 @@
 
           <DxColumn data-field="report_no" caption="Report number" />
 
-          <DxColumn data-field="id_campaign" caption="Campaign">
-            <DxLookup
-              :data-source="campaigeList"
-              value-expr="id_campaign"
-              display-expr="campaign_desc"
-            />
-          </DxColumn>
           <DxColumn data-field="remark" caption="Remark" />
 
           <!-- <DxColumn :width="80" caption="" cell-template="cell-button-set" /> -->
@@ -93,25 +171,15 @@
         <DxDataGrid
           id="data-grid-style"
           key-expr="id_inspection_record"
-          :data-source="dataList.cml"
+          :data-source="dataList.thk"
           :selection="{ mode: 'single' }"
           :hover-state-enabled="true"
           :allow-column-reordering="true"
           :show-borders="true"
           :show-row-lines="true"
           :row-alternation-enabled="false"
-          @exporting="EXPORT_DATA"
           :word-wrap-enabled="true"
-          @row-inserted="CREATE_RECORD"
-          @row-updated="UPDATE_RECORD"
-          @row-removed="DELETE_RECORD"
         >
-          <DxEditing
-            :allow-updating="true"
-            :allow-deleting="true"
-            :allow-adding="true"
-            mode="row"
-          />
           <!-- <DxColumn
         data-field="created_time"
         :width="0"
@@ -127,102 +195,6 @@
           />
 
           <DxColumn data-field="report_no" caption="Report number" />
-
-          <DxColumn data-field="id_campaign" caption="Campaign">
-            <DxLookup
-              :data-source="campaigeList"
-              value-expr="id_campaign"
-              display-expr="campaign_desc"
-            />
-          </DxColumn>
-          <DxColumn data-field="remark" caption="Remark" />
-
-          <!-- <DxColumn :width="80" caption="" cell-template="cell-button-set" /> -->
-
-          <!-- <template #cell-button-set="{ data }">
-        <div class="table-btn-group">
-          <div class="table-btn" v-on:click="EDIT_INFO(data)">
-            <i class="las la-pen blue"></i>
-          </div>
-          <div class="table-btn" v-on:click="DELETE_INFO(data)">
-            <i class="las la-trash red"></i>
-          </div>
-        </div>
-      </template> -->
-          <template #table-header>
-            <div>
-              <div class="page-section-label">Shell Course</div>
-            </div>
-          </template>
-          <!-- <template #table-header-button-set>
-        <div>
-          <v-ons-toolbar-button>
-            <i class="las la-plus"></i>
-            <span>Add New Tank Course</span>
-          </v-ons-toolbar-button>
-        </div>
-      </template> -->
-          <!-- Configuration goes here -->
-          <!-- <DxFilterRow :visible="true" /> -->
-          <DxScrolling mode="standard" />
-          <DxSearchPanel :visible="true" />
-          <DxPaging :page-size="10" :page-index="0" />
-          <DxPager
-            :show-page-size-selector="true"
-            :allowed-page-sizes="[5, 10, 20]"
-            :show-navigation-buttons="true"
-            :show-info="true"
-            info-text="Page {0} of {1} ({2} items)"
-          />
-          <DxExport :enabled="true" />
-        </DxDataGrid>
-      </div>
-      <div class="table-wrapper">
-        <DxDataGrid
-          id="data-grid-style"
-          key-expr="id_inspection_record"
-          :data-source="dataList.cml"
-          :selection="{ mode: 'single' }"
-          :hover-state-enabled="true"
-          :allow-column-reordering="true"
-          :show-borders="true"
-          :show-row-lines="true"
-          :row-alternation-enabled="false"
-          @exporting="EXPORT_DATA"
-          :word-wrap-enabled="true"
-          @row-inserted="CREATE_RECORD"
-          @row-updated="UPDATE_RECORD"
-          @row-removed="DELETE_RECORD"
-        >
-          <DxEditing
-            :allow-updating="true"
-            :allow-deleting="true"
-            :allow-adding="true"
-            mode="row"
-          />
-          <!-- <DxColumn
-        data-field="created_time"
-        :width="0"
-        caption=""
-        sort-order="asc"
-      /> -->
-          <DxColumn
-            data-field="inspection_date"
-            caption="Inspection date"
-            data-type="date"
-            format="dd MMM yyyy"
-            sort-order="desc"
-          />
-
-          <DxColumn data-field="report_no" caption="Report number" />
-
-          <DxColumn data-field="id_campaign" caption="Campaign">
-            <DxLookup
-              :data-source="campaigeList"
-              value-expr="id_campaign"
-              display-expr="campaign_desc"
-            />
-          </DxColumn>
           <DxColumn data-field="remark" caption="Remark" />
 
           <!-- <DxColumn :width="80" caption="" cell-template="cell-button-set" /> -->
@@ -269,31 +241,16 @@
         <DxDataGrid
           id="data-grid-style"
           key-expr="id_inspection_record"
-          :data-source="dataList.cml"
+          :data-source="dataList.thk"
           :selection="{ mode: 'single' }"
           :hover-state-enabled="true"
           :allow-column-reordering="true"
           :show-borders="true"
           :show-row-lines="true"
           :row-alternation-enabled="false"
-          @exporting="EXPORT_DATA"
           :word-wrap-enabled="true"
-          @row-inserted="CREATE_RECORD"
-          @row-updated="UPDATE_RECORD"
-          @row-removed="DELETE_RECORD"
         >
-          <DxEditing
-            :allow-updating="true"
-            :allow-deleting="true"
-            :allow-adding="true"
-            mode="row"
-          />
-          <!-- <DxColumn
-        data-field="created_time"
-        :width="0"
-        caption=""
-        sort-order="asc"
-      /> -->
+
           <DxColumn
             data-field="inspection_date"
             caption="Inspection date"
@@ -304,13 +261,6 @@
 
           <DxColumn data-field="report_no" caption="Report number" />
 
-          <DxColumn data-field="id_campaign" caption="Campaign">
-            <DxLookup
-              :data-source="campaigeList"
-              value-expr="id_campaign"
-              display-expr="campaign_desc"
-            />
-          </DxColumn>
           <DxColumn data-field="remark" caption="Remark" />
 
           <!-- <DxColumn :width="80" caption="" cell-template="cell-button-set" /> -->
@@ -364,7 +314,7 @@
 
 <script>
 //API
-// import axios from "/axios.js";
+import axios from "/axios.js";
 // import moment from "moment";
 
 //Components
@@ -386,7 +336,8 @@ import {
   //DxToolbar,
   //DxItem,
   DxEditing,
-  DxLookup,
+  //DxLookup,
+  DxButton,
 } from "devextreme-vue/data-grid";
 
 export default {
@@ -403,12 +354,12 @@ export default {
     //DxToolbar,
     //DxItem,
     DxEditing,
-    DxLookup,
+    //DxLookup,
+    DxButton,
   },
   created() {
     if (this.$store.state.status.server == true) {
-      // this.FETCH_TANK_INFO();
-      // this.FETCH_CLIENT_INFO();
+      this.FETCH_CML();
     }
   },
   data() {
@@ -422,8 +373,94 @@ export default {
     };
   },
   computed: {},
-  methods: {},
-};
+  methods: {
+    FETCH_CML() {
+      this.isLoading = true;
+      var id_tag = this.$route.params.id_tag;
+      axios({
+        method: "post",
+        url: "roof-thickness/roof-thk-cml-by-tank-id",
+        headers: {
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+        },
+        data: {
+          id_tag: id_tag,
+        },
+      })
+        .then((res) => {
+          console.log("cml:");
+          console.log(res.data);
+          if (res.status == 200 && res.data) {
+            this.dataList.cml = res.data;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    CREATE_CML(e) {
+      console.log(e);
+      this.isLoading = true;
+      var id_tag = this.$route.params.id_tag;
+      e.data.id_tag = id_tag;
+      e.data.id_cml = 0;
+      console.log(e);
+      axios({
+        method: "post",
+        url: "roof-thickness/add-roof-thk-cml",
+        headers: {
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+        },
+        data: e.data,
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.status == 200 && res.data) {
+            this.FETCH_CML();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    UPDATE_CML(e) {
+      console.log(e);
+      this.isLoading = true;
+      axios({
+        method: "put",
+        url: "roof-thickness/edit-roof-thk-cml",
+        headers: {
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+        },
+        data: e.data,
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.status == 200 && res.data) {
+            this.FETCH_CML();
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+    DELETE_CML(e) {
+      console.log(e);
+    },
+    VIEW_TP() {
+      console.log('in');
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
